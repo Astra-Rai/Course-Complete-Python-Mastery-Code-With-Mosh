c3a6b6731c986db9c17a16cc7819c8d6e6bc1509
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-05 22:50:37 -0400

    escape sequences:   \"    \'    \\    \n

---
+++ Python-2-Primitive-Types-4-Escape-Sequences.py
@@ -0,0 +1,23 @@
+#escape sequences
+
+# comments are not executed by python interpreter
+# examples of escape sequences in python# 
+# \"
+# \'
+# \\
+# \n
+
+# backslash, escape character, escape the character after
+# put quotes around characters in string using escape character
+# \"" , is an escape sequence
+course = " \" Python Programming \" "
+print(course)
+
+
+# have a back slash before a string 
+
+file_path = " \\Python\\Lecture-1\\Section-3"
+print(file_path)
+
+new_line = "Astra\n Rai"
+print(new_line)
