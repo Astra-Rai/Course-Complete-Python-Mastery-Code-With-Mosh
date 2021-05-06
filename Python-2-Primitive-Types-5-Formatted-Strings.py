13670097634d966779d09ba4d7d618b75d1ede58
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-05 23:24:31 -0400

    formatted strings,example:full = f"{first} {last}"

---
+++ Python-2-Primitive-Types-5-Formatted-Strings.py
@@ -0,0 +1,13 @@
+# Formatted-Strings 
+
+first = "Astra"
+last = "Rai"
+full = f"{first} {last}" # Astra Rai
+full_one = f"{len(first)} {2 + 2}" # 5 4
+print(full)
+print(full_one)
+
+# you can put any valid expressing in-between the {} braces
+
+# old way to concatenate vars using + operator and empty string, full = first + " " +  last
+# print(full)
