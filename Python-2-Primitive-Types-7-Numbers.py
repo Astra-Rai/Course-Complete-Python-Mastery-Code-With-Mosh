c91592e276e2837dfaf6b07efcc4ec865ad33308
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-06 14:06:52 -0400

    num types: int, float, complex, +  - * /  %  //

---
+++ Python-2-Primitive-Types-7-Numbers.py
@@ -0,0 +1,32 @@
+# numbers
+
+# types of numbers in python: intergers, floats and complex numbers 
+a = 1 # integer
+b = 1.1 # float
+
+
+# syntax for complex numbers
+# math synax x = a + bi, where ' i '' is an imiginary, in python 'j ' is used instead of i
+
+x = 1 + 2j # where j is the imiginary number
+
+# standard Arithmetic operators
+
+print(10 + 3) # addition, 13
+print(10 - 3) # subtraction, 7
+print(10 * 3) # multiplication, 30
+print(10 / 3) # division, 3.333...35, can get a float
+print(10 // 3) # division, 3,  interger only no floats 
+print(10 % 3) # modulus operator, 1, this is the remainder
+
+
+# argument assignment operator
+
+y = 10
+y = 10 + 3 # increment y by 3
+print(y) #13
+
+
+z = 15 
+z += 3 #increment z by three
+print(z) #18
