33baef51c8623ad0c45d4da32b64f2f2b3f16678
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-07 15:23:25 -0400

    if statement, code block, indent 4 spaces

---
+++ Python-3-Control-Flow-2-Condition-Operators-Part-1.py
@@ -0,0 +1,9 @@
+
+# if statement
+# Python-3-Control-Flow-2-Condition-Operators-Part-1.py
+
+temperature = 35
+if temperature > 30: 
+    print("It's warm") #  autopet8, will indent four spaces
+    print("Drink water")
+print("Done") 
