76433bb6edf88fe65fc485d75ce576fd59712516
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-07 15:31:20 -0400

    if, elif ( else if ), else

---
+++ Python-3-Control-Flow-2-Condition-Operators-Part-2.py
@@ -0,0 +1,20 @@
+
+# if statement
+# Python-3-Control-Flow-2-Condition-Operators-Part-2.py
+# if, elif ( else if ), else
+
+temperature = 15
+
+if temperature > 30: 
+    print("It's warm") #  autopet8, will indent four spaces
+    print("Drink water")
+
+elif temperature> 20:
+    print("It's nice") 
+
+# if none of the previous statements have the bool value True,  print else statment
+
+else:
+    print("It's cold")
+
+print("Done") 
