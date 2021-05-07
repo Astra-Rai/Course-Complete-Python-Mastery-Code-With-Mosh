6baa5275d9a7da893655a0872e000fba1abbc480
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-07 15:11:57 -0400

    >,   <,  <=,   >=,   !=,  ==,  ord

---
+++ Python-3-Control-Flow-1-Comparison-Operators.py
@@ -0,0 +1,12 @@
+
+
+print(10 > 3) # bool, True
+print(10 < 3) # bool, False
+print(10 == 10) #bool, True 
+print(10 == '10') # bool False
+print(10 != 10) # bool, False
+print("bag" > "apple") # bool, True, if we use sort, "bag" comes "after" apple
+print("bag" == "Bag") # False
+
+print(ord("b")) # ord() Return the Unicode code point for a one-character string, unicode for b is 98
+print(ord("B")) # unicode for B 66
