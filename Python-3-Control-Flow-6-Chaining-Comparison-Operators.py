a96fd2d90feb996fd6a509b4d9105577fe454729
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-08 16:44:04 -0400

    if 18 <= age < 65

---
+++ Python-3-Control-Flow-6-Chaining-Comparison-Operators.py
@@ -0,0 +1,8 @@
+
+# Python-3-Control-Flow-6-Chaining-Comparison-Operators
+# rule: age should be between 18 and 65 to print "Eligible" statement
+
+age = 22 
+# if age >= 18 and age < 65:
+if 18 <= age < 65: # this coding is cleaner and easier to read 
+    print("Eligible..")
