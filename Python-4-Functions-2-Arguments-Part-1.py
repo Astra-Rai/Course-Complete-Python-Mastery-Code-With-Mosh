81d3528774671619b20627f8aa80bbfc01539edb
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-15 17:52:05 -0400

    func w/parameters, args vs. parameters, fstring

---
+++ Python-4-Functions-2-Arguments-Part-1.py
@@ -0,0 +1,13 @@
+# functions: arguements
+# parameter: input you define for your function
+# function greet paramenters: #parameters:  (first_name, last_name)
+# function arguments: actual value for a given parameter
+# f: formatted string
+
+
+def greet(first_name, last_name):
+  print(f"Hi {first_name} {last_name}")
+  print("Welcome aboard")
+
+# arguments for the greet function: "Astra", "Rai"
+greet("Astra", "Rai")
