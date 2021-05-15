8f8438c1f925d138aff7be7dda7327022fd92fa1
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-15 17:40:08 -0400

    def keyword, indent cold block, call function

---
+++ Python-4-Functions-1-Defining-Functions.py
@@ -0,0 +1,18 @@
+#wriitng your own functions:
+    #functions should be saved in a seperate file
+    #you want functions to be maintainable and, if applicable, reuseable
+
+#functions should be: meaningful, descriptive....
+#use lowercase letters to name function _underscore to seperate multiple words
+
+#def keyword is used to define , colon:
+#indent code that relates to function
+#add two line breaks after code in function
+
+
+def greet():
+    print("Hi there")
+    print("Welcome aboard")
+
+
+greet()
