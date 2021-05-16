2d7c86636e475acfdee554bfe6b71ae2920dfde2
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-16 03:24:22 -0400

    fun() return value not specified, "None"

---
+++ Python-4-Functions-3-Types-of-Functions-Part-4.py
@@ -0,0 +1,13 @@
+# function returning 'None'
+# even though fun returns 'None' by default, it is still classified as a function  that carries out a task
+# 'None' is a obj that represents the absence of a value 
+# all functions return 'None' by default, unless you specifically return a value 
+
+def greet(name): 
+  print (f"Hello {name}")
+
+greet("Astra Rai")  
+print(greet("Astra Rai"))
+print(type(None)) # class ????
+
+# date May 16, 2021, I don't fully understand the concept behind "None"...review again in a few days
