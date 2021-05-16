24e975c23a2df60db4b2198ada1ed0569c371038
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-16 01:50:37 -0400

    Delete Python-4-3-Types-of-Function-Part-1.py

--- Python-4-3-Types-of-Function-Part-1.py
+++
@@ -1,9 +0,0 @@
-# types of functions
-  #1 function that performs a task
-  #2 function that returns a ValueError
-  
-def greet(name): # function performs a task
-  print(f"Hi {name}")
-greet("Astra")
-
-print(round(1.9))# function calculates and returns a value
