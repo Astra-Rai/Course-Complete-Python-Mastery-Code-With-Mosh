da7a6adce642849fb699b31d0722c85ba4ffac3c
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-16 01:46:51 -0400

    2 types of fun: perform task, calc & return value

---
+++ Python-4-Functions-3-Types-of-Function-Part-1.py
@@ -0,0 +1,9 @@
+# types of functions
+  #1 function that performs a task
+  #2 function that returns a ValueError
+  
+def greet(name): # function performs a task
+  print(f"Hi {name}")
+greet("Astra")
+
+print(round(1.9))# function calculates and returns a value
