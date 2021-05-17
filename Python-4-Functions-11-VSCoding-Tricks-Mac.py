8b81ec0a08c6aa1981a11dc6bc6b9859fbc6d099
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-17 00:29:07 -0400

    moving code, comments, moving cursor w arrow keys

---
+++ Python-4-Functions-11-VSCoding-Tricks-Mac.py
@@ -0,0 +1,16 @@
+
+# move cursor
+# FN + up arrow, top
+# FN + down arrow, bottom
+# FN + left arrow, left margin of line
+# FN + right arrow, same line, end of line to the right
+
+
+# moving lines not using copy and paste - this is dope!
+
+# select line(s), ALT + up arrow or ALT + down arrow
+
+# adding and remove comments
+# select text: protable keyboard (logitech) using a Mac Pro 13, command (windows icon)
+
+
