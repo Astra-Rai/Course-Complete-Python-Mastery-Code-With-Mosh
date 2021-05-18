4416699c94df056fa08705e1c470f04b7928bab7
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-17 23:29:24 -0400

    division / float, // whole #, augment operator

---
+++ Python-3-Control-Flow-12-While-Loops-Part-1.py
@@ -0,0 +1,20 @@
+
+# while loops: used to repeat something as long as a condition is true
+
+# example 1
+# while number is greater than zero, if condition is true, divide 'number' by 2in each interation? divide by half
+# 100/2 = 50, 50/2 = 25, 25/2= 15.5
+
+number = 100 #define a variable 'number' and set it to 100
+# while looop used to evaluate a condition and repeat a task
+while number > 0: # while number is greater than zero, divide number
+  print(number) # with indentation, we can repeat 1 or more statements
+  number //= 2 # same as: number = number // 2, use agumented assignment operator to shorten code
+
+# In Python, there are two types of division operators:
+
+# / : Divides the number on its left by the number on its right and returns a floating point value.
+
+# // : Divides the number on its left by the number on its right, rounds down the answer, and returns a whole number.
+
+# source: https://www.educative.io/edpresso/what-are-division-operators-in-python
