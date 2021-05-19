d9f12851447d96bbce647c3badcb6ba545eb5f33
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-19 02:19:02 -0400

    if statement, and operator, print()

---
+++ Python-3-Control-Flow-4-Logical-Operators-Part-1.py
@@ -0,0 +1,14 @@
+# Logial Operators
+# Use of: if statement, logical operator 'and'
+# In python we have 3 complex operators
+# Operators used to model more complex conditions: and, or, not
+
+# Example: Buidling an application processings
+# Loan is processed if client has high_income and good_credit. Both represent by boolean value: 'True'
+
+high_income = True
+good_credit = True
+
+if high_income and good_credit:
+    print("Eligible")
+
