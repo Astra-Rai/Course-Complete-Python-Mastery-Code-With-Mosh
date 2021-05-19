54808096120b5dbdae8f6b50a41bf0086d1dec9e
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-19 13:39:14 -0400

    for in, range(start, stop, step), print()

---
+++ Python-3-Control-Flow-8-For-Loops-Part-1.py
@@ -0,0 +1,15 @@
+
+# for in loop, range() function
+# exercise 1: send message to your, if msg not delivered, try 3x'set
+# A for loop is used for iterating over a sequence
+
+for number in range(3): # start at 0 and stop before three
+#number is <class 'int'>, number increments by one with each interation
+
+  print("Attempt")
+
+#output: vertically: Attempt Attempt  Attempt
+
+# range() function returns a sequence of numbers..
+# starting from 0 by default
+# and increments by 1 (by default), and stops before a specified number.
