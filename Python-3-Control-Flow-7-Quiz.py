24d1f5e13f2ccc6d1f7cae12295b482a8e2da8d7
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-19 03:33:21 -0400

    if, elif, else, ==, sort(), > , < , ord()
    Read up on this: https://stackoverflow.com/questions/4806911/string-comparison-technique-used-by-python

---
+++ Python-3-Control-Flow-7-Quiz.py
@@ -0,0 +1,15 @@
+
+#Python-3-Control-Flow-7-Quiz.py
+# The sort() method sorts the elements of a given list in a specific ascending or descending order.
+# read this in stack overflow: https://stackoverflow.com/questions/4806911/string-comparison-technique-used-by-python
+
+if 10 == '10': # not TRUE: 10 is and integer WHEREAS '10' is data type string
+    print("a")
+elif "bag" > "apple" and "bag" > "cat":
+    print("b")
+else:
+    print("c")
+
+print(ord('a')) # output 97
+print(ord('b')) # output is 98, 98 > 97 bag is greater than apple
+print(ord('c')) # output is 99 cat is greater than bag ...if sort was used??? # I need to study this. check out the stack overflow link
