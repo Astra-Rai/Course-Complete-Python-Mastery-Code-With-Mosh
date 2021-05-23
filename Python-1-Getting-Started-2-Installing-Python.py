41e6603fccb9fbbc6161e719fa6a07948f148972
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-23 02:00:28 -0400

    Installing Python

---
+++ Python-1-Getting-Started-2-Installing-Python.py
@@ -0,0 +1,17 @@
+
+
+# Terminal: Check Python version: Python --version
+
+# After I installed Python, if I typed python3 in terminal and pressed enter, this is the output (note hashtages not included)
+    #Python 3.9.4 (v3.9.4:1f2e3088f3, Apr  4 2021, 12:19:19) 
+    #[Clang 12.0.0 (clang-1200.0.32.29)] on darwin
+    #Type "help", "copyright", "credits" or "license" for more information.
+    #>>> 
+
+# Download Python: https://www.python.org/downloads/
+
+# select checkbox: add python 3.7 to path, follow wizard to install
+
+# terminal
+# exit control z
+# control d, to verify that python 3.7. session will save and log out of terminal
