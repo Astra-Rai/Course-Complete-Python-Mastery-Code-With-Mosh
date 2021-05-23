d3a496656cdf959e58f43353bf85b440fd62723e
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-23 15:19:12 -0400

    Linting

---
+++ Python-1-Getting-Started-7-Linting-Python-Code.py
@@ -0,0 +1,14 @@
+#  error example
+#  print "Hello World"
+#  red underline: Error Message: missing() in call to print
+
+#  2 + 
+#  pylint message: invalid syntax
+
+
+# view -> problems
+
+# view -> command pallete: lint, all commands related to linting, all start with python
+
+# different linters: flake8, mypy, pep8, prospector, pydocstyle, pylama, pylint
+# pylint is the default linter in the Python extension for VSCode
