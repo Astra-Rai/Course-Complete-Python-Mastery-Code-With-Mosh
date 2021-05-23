d17c60809b0fcd0f84297b237a598468db579e9a
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-22 22:36:38 -0400

    Basics of accessing list items,  []

---
+++ Python-5-Data-Structures-2-Accessing-Items-Part-1.py
@@ -0,0 +1,9 @@
+
+letters = ["a", "b", "c","d"]
+
+# print(letters[0])# return 1st items in list: "a"
+print(letters[-1]) # returns the first item in end of list: "d"
+print# use [] square brackets to modify items in list, change 1st item to 
+
+letters[0] = "A"
+print(letters) # output: ['A', 'b', 'c', 'd']
