f505dbb7571ea67372766b287bad8d7b2d30ce92
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-23 02:05:17 -0400

    Python Interpreter

---
+++ Python-1-Getting-Started-3-Python-Interpreter.py
@@ -0,0 +1,12 @@
+
+# Mac Pro
+# Open Terminal
+# Type python3, press enter
+# Now we're using interactive shell, we can type an expession
+# In programming, an expression is a piece of code that produces a value
+# Synax is grammar in programming
+# This interactive shell is a great way to interact with a bit of Python code but...
+# This is not how we build real world applications
+# To build real world applicaitons, we need a code editor
+
+
