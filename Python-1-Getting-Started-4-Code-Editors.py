629f3f1d6638fe8c3c9ebc76e0cc59f03d2361b5
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-23 02:12:54 -0400

    Code Editors

---
+++ Python-1-Getting-Started-4-Code-Editors.py
@@ -0,0 +1,12 @@
+
+# Two tools for typing Python code"
+
+# 1. Code Editor
+# 2. IDE: Integrated Development Environment 
+
+# IDE: code editor w/ autocompletion, debugging, testing
+# Most popluar code editors: VSCode, Atom, Sublime
+# IDE most popular IDE is Pycharm
+# Later in lesson, Mosh will show us how to install a plugin / extension that will convert VSCode to a powerful IDE
+# For this course we'll use VSCode
+# VSCode download link: https://code.visualstudio.com/download
