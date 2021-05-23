138da3aaa680c2ee465ed021ceed91f7f45f873a
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-23 15:46:36 -0400

    Formatting Code, autopep8

---
+++ Python-1-Getting-Started-8-Formatting-Code.py
@@ -0,0 +1,18 @@
+# Python Enhancement Proposals (PEPs), Link: https://www.python.org/dev/peps/
+# Pep 8: Style Guide for Python Code
+
+# : tool to automatically format code and code into PEP 8
+
+x = 1  # declaring a variable and setting to one
+# there should be a space before and after the = sign
+# go to command palette, view -> command palette
+# type format, then press enter
+# msg: Formatter autopep8 not installed. Install? Source: Python (Extension)
+# Python extension that we installed uses autopep8 to format our code
+
+
+# code menu -> preferences-> settings-> search box -> searc for 'format', select 'format on save'
+# ...maybe try 'format on type' later
+
+x = 4
+
