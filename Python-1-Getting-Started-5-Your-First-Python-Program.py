3bc937bdfcdfba7eb1e56d7cb85c2efa08f95a18
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-23 14:58:03 -0400

    VSCode, Hello World, print()

---
+++ Python-1-Getting-Started-5-Your-First-Python-Program.py
@@ -0,0 +1,13 @@
+
+# to open file, click on the explore icon the upper left corner, looks like to papers
+# to open terminal: ctrl button and back tick `, this is our integrated terminal
+# run code: type python, version number, space the file name
+# run code example: python3 Python-1-Getting-Started-5-Your-First-Python-Program.py
+# mac or linux, type: python3, windows python
+
+# print function, prints something on the screen
+# calling a function means excuting
+# code is executed from top to bottom
+
+print("Hello World")
+print("*" * 10)
