26a38f375e4781c8589da7318c986dcf18ed5d17
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-23 15:10:03 -0400

    Python Extensition, Pylint

---
+++ Python-1-Getting-Started-6-Python-Extensions.py
@@ -0,0 +1,13 @@
+
+# Converting VSCode to a powerful IDE
+# IDE: Integrated Development Environment, Benefits listed below:
+
+    # Linting: analyzing code for errors
+    # Debugging: finding and fixing errors
+    # Autocompletion: helps write code faster, also less mis spells
+    # Code formating: making code clean and readable
+    # Unit Text: writing text, running them in automated fashion to ensure our code is behaving correctly
+    # Code Snippets: resuseable code blocks that we can quickly regenerate
+
+# install the python extension and reload VSCode
+# Message: Linter pylint not installed, click on msg to install
