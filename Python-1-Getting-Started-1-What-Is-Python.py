41be1099ba0310ef86c0da4956623ece2cc780f2
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-23 01:52:07 -0400

    What is Python

---
+++ Python-1-Getting-Started-1-What-Is-Python.py
@@ -0,0 +1,15 @@
+
+
+#  Python is the world's fastest growing languages
+#  Why is Python Ideal?:
+    # 1.) Solve problems in less time and less code
+    # 2.) Is a multi-purpose language: data analysis, AI and Machine Learning..
+    #     Writing Automation Scripts, building web, mobile and desktop applications, software testing, hacking
+    # 3.) Is a high-level language; we dont' have to worry about complex tasks such as memory management 
+
+    # 4.) Crosx-platform: we can build and run Python apps on : Windows, Mac, Linux
+    # 5.) Huge community
+    # 6.) Large ecosystem of frameworks, libraries and tools
+
+
+# 2 Versions of Ptyhon: 2v Legacy, 3v newest 
