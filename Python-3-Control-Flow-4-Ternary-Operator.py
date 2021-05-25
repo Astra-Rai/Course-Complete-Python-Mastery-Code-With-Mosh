587ee3788929edab187f33d0ae25792ec781acdf
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-25 00:33:31 -0400

    if else, assigning variable use ternary operator

---
+++ Python-3-Control-Flow-4-Ternary-Operator.py
@@ -0,0 +1,17 @@
+
+# Python-3-Control-Flow-3-Ternary-Operator.py
+
+age = 22
+if age >= 18:
+    # not the cleanest way to write the code: print("Eligible")
+    message = "Eligible"
+else:
+    messge = "Not eligible"  # print("Not eligible")
+
+print(message)
+
+# use ternary operator
+
+# message = "Eligible if age >=18 else ""Not eligible"
+
+# message = "Eligible" if age >=18 else "Not eligible"
