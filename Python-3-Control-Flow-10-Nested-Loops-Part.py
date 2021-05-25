675b580f090e745b9cdec3c1de2520e2f694c092
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-25 01:42:58 -0400

    for in, nested loop, child of for statement

---
+++ Python-3-Control-Flow-10-Nested-Loops-Part.py
@@ -0,0 +1,26 @@
+
+
+# nest loops: one loop inside another
+
+for x in range(5): # outer loop
+    for y in range(3): # inner loop # this statement is child of for statement
+        print(f"({x}, {y})")  # use formatted strings to display coordinates
+
+# how python interprets the code: watch video again, don't get stuck, it will click eventually
+# code output: 
+
+(0, 0)
+(0, 1)
+(0, 2)
+(1, 0)
+(1, 1)
+(1, 2)
+(2, 0)
+(2, 1)
+(2, 2)
+(3, 0)
+(3, 1)
+(3, 2)
+(4, 0)
+(4, 1)
+(4, 2)
