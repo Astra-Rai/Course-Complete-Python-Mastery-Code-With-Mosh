446b5ef19e66a6f6c8fc9fd79f150a06c21fd485
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-24 23:08:44 -0400

    byte code, complier, VM, machine code explained

---
+++ Python-1-Getting-Started-11-How-Python-Code-Is-Executed.py
@@ -0,0 +1,19 @@
+
+
+# How Python Code is Executed
+
+# C Complier converts C code into Machine Code
+# Machine code is specific to the type of processor of a computer ....
+# If C code is complied on windows, cannot execute on Mac
+# Java complier complier complies code into Java Bytecode which is not specific to a hardware platform, like Windows or Mac
+# Java code/program -> complier -> Java Bytecode -> Java Virtual Machine -> Machine Code
+# JVM loads Java byte code and at run time, it converts each instruction to Machine code ...
+# This allows Java byte code on any platform that hhas JVM
+# C# and Pyton are platform independent 
+# How CPython Works..
+# Python program / code, -> Cypthon -> Python Bytecode -> Python VM - Machine Code
+# Python implementations: Jython: Java, ItonPython: C#, PyPy: Subset of Pythoon
+# Jython: instead of instead of compling python code into python byte code, it's complied to Java byte code ..
+# Java byte code and run it using Java VM, Java byte code will eventually execute by JVM
+
+
