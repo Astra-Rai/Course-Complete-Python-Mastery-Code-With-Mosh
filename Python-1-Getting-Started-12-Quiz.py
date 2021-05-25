9f984c90fb881d7ab4f155c46851e067df2c06f9
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-24 23:41:58 -0400

    What is: an expression, syntax error, linter?

---
+++ Python-1-Getting-Started-12-Quiz.py
@@ -0,0 +1,9 @@
+
+
+# an expression is a piece of code that produces a value 
+#  "*" * 3, output: ***
+
+# an syntax error is due to bad syntax, bad grammar used in the code
+
+# a linter is a tool that checks our code for errors, mostly syntactial
+
