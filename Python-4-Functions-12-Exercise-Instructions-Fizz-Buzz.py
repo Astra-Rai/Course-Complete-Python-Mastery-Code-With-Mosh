b08ca6f3e8d01cb0e012bdaf0c07407dcdb3d7dc
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-26 00:36:08 -0400

    Exercise Instructions: Fizz Buzz

---
+++ Python-4-Functions-12-Exercise-Instructions-Fizz-Buzz.py
@@ -0,0 +1,7 @@
+# Exercise Instructions
+
+# function that takes and inut
+# if input is divisible by 3 it will return the string fizz
+# if input is divisible by 5 it will return the string buzz
+# if input by both 3 and five it will return fizz buzz
+# for any other number, it will return to the input
