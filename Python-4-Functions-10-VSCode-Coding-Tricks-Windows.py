644d79d69e87727001671c8905719f5649e8bf54
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-26 00:15:58 -0400

    Windows shortcuts

---
+++ Python-4-Functions-10-VSCode-Coding-Tricks-Windows.py
@@ -0,0 +1,11 @@
+
+# Windows Shortcuts
+
+# move cursor from beginning to end of line: press end key
+# beginning of line, press home key
+# control + home, move curson to beginning of file
+# control + end or n??
+# instead of copy paste, select, alt and use up or down arrows
+# dublicate multip lines, select, shift, alt down
+# comment two line - press control and /, using same shortcut, # removed, you can toggle it
+# typing variable: type first few characters, pic any letters in sequence and type and press enter, autocompletion
