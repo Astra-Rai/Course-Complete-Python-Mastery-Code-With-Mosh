64bd9c17b82152e2985d4558225e20643a3e54a3
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-27 14:40:55 -0400

    index(),value error if search item not in index

---
+++ Python-5-Data-Structures-6-Finding-Items-Part-2.py
@@ -0,0 +1,5 @@
+# The index() method returns the position at the first occurrence of the specified value.
+
+letters = ["a", "b", "c"]
+print(letters.index("a")) # output:     0
+print(letters.index("z")) # output:     ValueError: 'z' is not in list
