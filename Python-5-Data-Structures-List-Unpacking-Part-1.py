dacc090dd1ba5c944d203751f3356fe8cbc90724
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-26 22:44:07 -0400

    unbalance tuple unpacking

---
+++ Python-5-Data-Structures-List-Unpacking-Part-1.py
@@ -0,0 +1,19 @@
+
+
+# when you want to get individuals items in a list and assign them to a variable
+# number of variables on the lest side of assignment operator should be equal to # of items in list
+
+numbers = [1, 2, 3]
+
+# example,  error: unbalance tuple unpacking
+
+first, second = numbers
+
+#   example 1: 
+#       numbers = [1, 2, 3]
+#       first = numbers[0]
+#       second = numbers[1]
+#       third = numbers[2]
+
+#       print(numbers)
+
