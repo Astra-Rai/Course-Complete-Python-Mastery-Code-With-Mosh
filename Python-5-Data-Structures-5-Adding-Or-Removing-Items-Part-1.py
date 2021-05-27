8b9c02f4df7b2a929f4b848431cfa9bcfb49c969
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-26 23:46:16 -0400

    add items to list, append(), insert()

---
+++ Python-5-Data-Structures-5-Adding-Or-Removing-Items-Part-1.py
@@ -0,0 +1,9 @@
+
+letters = ["a", "b", "c"]
+
+# Add items to list
+
+letters.append("d")         # add item to end of list 
+letters.insert(1,"-")       # insert dash at index 1
+
+print(letters) # output:    ['a', '-', 'b', 'c', 'd']
