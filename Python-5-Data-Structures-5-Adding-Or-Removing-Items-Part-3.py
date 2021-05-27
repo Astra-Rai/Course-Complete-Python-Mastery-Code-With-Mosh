754e91b78813f8263e2630f3ee328c6667b9f22d
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-27 00:09:13 -0400

    pop(), remove item ast specified index

---
+++ Python-5-Data-Structures-5-Adding-Or-Removing-Items-Part-3.py
@@ -0,0 +1,8 @@
+
+letters = ['a', '-', 'b', 'c', 'd']
+
+# remove item at a specified position, in this exampe at the idex 2, letters[2] = 'b'
+
+letters.pop(2)  # before pop()method  ['a', '-', 'b', 'c', 'd']
+print(letters)  # output ['a', '-', 'c', 'd']
+
