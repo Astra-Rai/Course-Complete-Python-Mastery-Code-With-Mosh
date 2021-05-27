ce63a9b6c0a55c1609ff9e0b79e07bc8b800c9e2
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-27 15:04:33 -0400

    count() number of occurreces of item list
    occurrences

---
+++ Python-5-Data-Structures-6-Finding-Items-Part-4.py
@@ -0,0 +1,7 @@
+#  The 'in' operator is used to check if a value exists in a sequence or not
+
+letters = ["a", "b", "c", "d", "a"]
+
+print(letters.count("d"))  # output: 1
+print(letters.count("a"))  # output: 2
+print(letters.count("z"))  # output: 0
