afbd3e48e89f020e8ab37183420c576bbbf6e41a
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-26 23:15:48 -0400

    enumerate() method, tuples

---
+++ Python-5-Data-Structures-Looping-Over-Lists-Part-1.py
@@ -0,0 +1,10 @@
+# get index of each item and item paired in touble
+
+letters = ["a", "b", "c"]
+for letter in enumerate (letters):  
+    print(letter)
+
+#   output:
+#       (0, 'a')
+#       (1, 'b')
+#       (2, 'c')
