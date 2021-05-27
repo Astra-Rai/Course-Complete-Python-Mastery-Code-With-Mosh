636b7a9c0c16524ff506e9d10cf5f583e9889ccf
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-26 23:58:26 -0400

    remove item from end of list, pop()

---
+++ Python-5-Data-Structures-5-Adding-Or-Removing-Items-Part-2.py
@@ -0,0 +1,7 @@
+
+letters = ['a', '-', 'b', 'c', 'd']
+
+# remove item from end of list
+
+letters.pop()   #   before pop()method  ['a', '-', 'b', 'c', 'd']
+print(letters)  #   output ['a', '-', 'b', 'c']
