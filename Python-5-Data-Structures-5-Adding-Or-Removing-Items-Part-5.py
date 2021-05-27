2636a6f574959419bab1d82932ab7843914e0a92
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-27 00:18:09 -0400

    del keyword, delete range of list items

---
+++ Python-5-Data-Structures-5-Adding-Or-Removing-Items-Part-5.py
@@ -0,0 +1,8 @@
+
+letters = ['a', '-', 'b', 'c', 'd']
+#          0     1    2    3    4
+
+del letters[0:2] #  delete letters, start at 0 index end before index 2
+print(letters) #    output: ['b', 'c', 'd']
+
+
