914d349dc00cc6313bce9985a5d9f4c4455b569f
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-27 00:09:46 -0400

    remove() method

---
+++ Python-5-Data-Structures-5-Adding-Or-Removing-Items-Part-4.py
@@ -0,0 +1,11 @@
+
+letters = ['a', '-', 'b', 'c', 'd']
+
+# remove letter from list
+
+    # example:      letters.remove(a)   output: ['-', 'b', 'c', 'd']
+
+# attempt to remove element not in list
+
+    # example:          letters.remove("z") #  
+    #if you try to remove a element not in list: Value Error: list.remove(x): x not in list
