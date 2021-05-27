a78408e62d434433a10c738e1149fa1b763779d3
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-26 23:18:57 -0400

    enumerate, print(letter[0]), index only

---
+++ Python-5-Data-Structures-Looping-Over-Lists-Part-2.py
@@ -0,0 +1,11 @@
+# get index of each item and item paired in touble
+
+letters = ["a", "b", "c"]
+for letter in enumerate(letters):
+    print(letter[0])
+
+
+#   output:
+#       0
+#       1
+#       2
