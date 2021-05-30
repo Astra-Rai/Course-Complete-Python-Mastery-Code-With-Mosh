9d5b4c75b55e31304463514f55953766931b00f0
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-29 23:44:48 -0400

    (input % 3==0)and(input % 5==0) #1st block of code

---
+++ Python-4-Functions-13-Solution-Part-2.py
@@ -0,0 +1,27 @@
+
+def fizz_buzz(input):
+
+    # if input evenly divisible by 3 and 5, return the string "Fizz Buzz"
+    if (input % 3 == 0) and (input % 5 == 0):
+        # not if this line is last code block, and it's true, code on line 3 or 5 will print first...
+        return "Fizz Buzz"
+        # and code stops being executed
+
+    if input % 3 == 0:  # if input is divisible by 3 w/o a remainder, return the string "Fizz"
+        return "Fizz"
+    if input % 5 == 0:  # if input is divisible by 5 w/o a remainder, return the string "Buzz"
+        return "Buzz"
+
+    else:
+        return "{} : Not evenly divisible by 3, 5, nor simultaneously evenly dividle by 3 and 5".format(input)
+
+
+print(fizz_buzz(7))
+
+
+# I expected this to return the string'Fizz Buzz' because 15 is evenly divisible by 3 and 5
+# But, the code is not ran once the first statement evalutes as true
+
+# first version, if input % 3 == 0: return "Fizz", else return "Buzz"
+# doesn't take in account if input is evenly divisible by 3 and 5
+# if the value returned is a string, elif, else not necessary
