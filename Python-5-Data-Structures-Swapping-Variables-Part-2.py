9242cc77153cf11bc2b46542d677f711c491f134
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-06-24 01:41:14 -0400

    swap variable using tuples

---
+++ Python-5-Data-Structures-Swapping-Variables-Part-2.py
@@ -0,0 +1,13 @@
+#   swap two variables by assigning a tuple
+
+x = 10  # print(x), output: 10
+y = 11  # print(y), output: 11
+
+# on right side of = operator a tuple is being defined
+
+x, y = y, x
+
+print("x", x)
+print("y", y)
+
+# Python-5-Data-Structures-Swapping-Variables-Part-2
