c87e4eaabfd5e1a172af47814a6db56c27973664
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2022-02-02 18:18:12 -0500

    Tuples Introduction

---
+++ Python-5-Data-Structures-Tuples-Part-1.py
@@ -0,0 +1,25 @@
+
+
+#point = () #empty tuple
+#print(type(point)) # output <class 'tuple'>
+#point = 1 NOTE, python things this is an integer, there leave a trailing comman
+
+
+#point = (1, 2) + (3,4) # concatenate two tuples, addition operator
+#print(point) output: (1, 2, 3, 4)
+
+
+#point = (1,2)*3 # multiplication operator used to repease tuple
+#print(point) output: (1, 2, 1, 2, 1, 2)
+
+#convert list to tuple
+#call tuple function
+#tuple to two numbers
+
+#point = (tuple[1,2]) output: (1,2)
+# remember strings are iterable, so output is a tuple of 11 strings
+#point = tuple('Hello World')
+#print(point) # output: ('H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd')
+
+#return a range (1,2,3), start at zero index stop before second index
+
