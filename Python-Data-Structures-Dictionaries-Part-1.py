cc0a8cd8250b3c94a1813c9bca7b793cf27a3277
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2022-02-23 00:32:58 -0500

    Dictionaries, key/value pairs, print key value

---
+++ Python-Data-Structures-Dictionaries-Part-1.py
@@ -0,0 +1,8 @@
+
+
+### dictionary is a collection of key/value pairs
+### dictionaries only use immutable types for keys, often strings and numbers used
+
+point = {"x":1, "y":2} #output = 1
+
+print(point["x"])
