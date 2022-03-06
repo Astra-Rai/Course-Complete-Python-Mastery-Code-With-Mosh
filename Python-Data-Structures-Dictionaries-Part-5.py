ba69042fe7450930276a82e90f3ec442a3491d3d
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2022-03-06 12:21:11 -0500

    iterate through a dictionary using a for loop

---
+++ Python-Data-Structures-Dictionaries-Part-5.py
@@ -0,0 +1,17 @@
+
+
+point = dict(x=1, y=2, z=20)
+# iterate through a dictionary 
+
+for key in point:
+        print(key, point[key]) #print key and key value 
+
+"""
+output 
+
+x 1
+y 2
+z 20
+
+"""
+
