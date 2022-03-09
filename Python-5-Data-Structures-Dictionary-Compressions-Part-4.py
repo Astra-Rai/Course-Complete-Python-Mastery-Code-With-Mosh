d1f7319f7f83ea6fc48272452391ae04fcab1d1f
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2022-03-09 00:38:55 -0500

    comprehension expressions to make dict {}

---
+++ Python-5-Data-Structures-Dictionary-Compressions-Part-4.py
@@ -0,0 +1,13 @@
+# dictionaries have key value pairs seperated by using a colon
+# 1 mapped to string of characters, "astra"
+# 2 mapped to string of characters, "rai"
+# 3 mapped to string of characters, "daniels"
+
+# use comprehesion expressions to make dictionary objects
+
+
+names = {1: "astra", 2: "rai", 3: "daniels"}
+print(names)
+
+
+
